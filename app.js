690f208b9441465547e947f40737c2c48c146c27
Rahul Raj <rahulraz1308@gmail.com>
2021-10-06 19:47:41 +0530

    minor change

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use('/api/v1/order', orderedProductRouter)
 app.use('/api/v1/payment', paymentRouter)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, './client/build')))
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
   })
 }
 
