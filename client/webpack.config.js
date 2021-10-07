1a137dfc00d5da997235b777a9c687f458195559
Rahul Raj <rahulraz1308@gmail.com>
2021-10-07 20:43:13 +0530

    webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,56 +3,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	mode: process.env.NODE_ENV,
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'main.bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					// options: {
-					// 	presets: ['@babel/preset-env'],
-					// },
-				},
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					process.env.NODE_ENV !== 'production'
-						? 'style-loader'
-						: MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg|ttf|woff|otf)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[contenthash].[ext]',
-							outputPath: 'static/img',
-						},
-					},
-				],
-			},
-		],
-	},
-	devtool: 'inline-source-map',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: 'index.html',
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-		}),
-	],
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|woff|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'static/img',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 }
